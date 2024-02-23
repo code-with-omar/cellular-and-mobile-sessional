ba45a45929c214144e7f1fb51768d447ecf7bbc8
Omar Faruk <omarfaruk65142@gmail.com>
2024-02-23 17:52:59 +0600

    Complete the 5th number lab

---
+++ lab-5.py
@@ -0,0 +1,52 @@
+import math
+
+# Given values
+area = 1300
+radius = 4
+# The area of a cell (hexagon) can shown to be 2.5981R^2
+
+each_cell_covers = math.floor(2.5981 * radius**2)  # in square kilometers
+# (A) Number of cells in the service area
+print("(a)")
+number_of_cells = math.floor(area / each_cell_covers)
+print("Number of cells:", number_of_cells)
+# (B) the number of channels per cell,
+
+allocated_spectrum = 40000
+channel_width = 60
+frequency_reuse_factor = 7
+print("(b)")
+number_of_channel_per_cell = math.floor(
+    allocated_spectrum / (channel_width * frequency_reuse_factor)
+)
+print("Number of channels per cell:", number_of_channel_per_cell)
+
+
+# (C)traffic intensity of each cell,
+print("(c)")
+traffic_intensity_per_cell = 84  # from erlang chart B
+print("Traffic intensity per cell:", traffic_intensity_per_cell)
+# (D) the maximum carried traffic;
+print("(d)")
+maximum_carried_traffic = number_of_cells * traffic_intensity_per_cell
+print("Maximum carried traffic:", maximum_carried_traffic)
+
+# (E) the total number of users that can be served for 2% GOS
+traffic_per_user = 0.03
+print("(e)")
+total_number_of_user = maximum_carried_traffic / traffic_per_user
+print("Total number of users:", total_number_of_user)
+
+# (F)  the number of mobiles per channel
+number_of_channels = number_of_channel_per_cell * frequency_reuse_factor
+print("(f)")
+number_of_mobile_per_channel = math.floor(total_number_of_user / number_of_channels)
+print("Number of mobiles per channel:", number_of_mobile_per_channel)
+print("(g)")
+theoretical_maximum_number_of_user_that_could_be_served = (
+    number_of_cells * number_of_channel_per_cell
+)
+print(
+    "Theoretical maximum number of users that could be served:",
+    theoretical_maximum_number_of_user_that_could_be_served,
+)
